9577dc2bc1d88c71da05bc93b3016d320275905a
victorch2023 <chowwha@gmail.com>
2023-09-11 02:14:08 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { useFormik } from "formik";
+
+function App() {
+  const formik = useFormik({
+    initialValues:{
+      email:"",
+      password:"",
+    },
+    onSubmit: (values) => {
+      alert("Login Successful");
+    },
+    validate: (values) => {
+      let errors = {};
+      if(!values.email) errors.email = "field required";
+      else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) 
+      errors.email = 'Username should be an email';
+      if(!values.password) errors.password = "field required";
+      return errors;
+    }
+  })
+
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Email:</div>
+        <input
+          id="emailField"
+          type="text"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        {formik.errors.email ? (
+          <div id="emailError" style={{ color: "red"}}>
+            {formik.errors.email}
+          </div>
+        ) : null}
+      
+        <div>Password:</div>
+        <input
+          id="pswField"
+          type="text"
+          name="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+       {formik.errors.password ? (
+          <div id="pswError" style={{ color: "red"}}>
+            {formik.errors.password}
+          </div>
+       ) : null}
+        <button id="submitBtn" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
